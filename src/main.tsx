d2152d77b3d48a64a6e412a04e2b0209bc8d4fe3
Geovane Filho <geo.vane.20002@gmail.com>
2023-01-26 14:34:15 -0300

    fix: project layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Sidebar from "./components/Sidebar";
-import "./index.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Sidebar />
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
