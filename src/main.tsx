808ed680f8d48f9c4198fdf294ee3db1a79434e2
Geovane Filho <geo.vane.20002@gmail.com>
2023-01-26 14:09:17 -0300

    chord: sidebar updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Sidebar from "./Sidebar";
+import Sidebar from "./components/Sidebar";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
